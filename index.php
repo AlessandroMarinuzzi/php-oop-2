aed3cd1ee5023846099ed5d43be33e802496af3a
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-06-28 15:51:23 +0100

    Init User and related subclass.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+#Product classes instances
 class Product{
     protected $brand;
     protected $name;
@@ -33,3 +33,36 @@ class Laptop extends Product{
 
 $lenovoThinkpad = new Laptop ("Lenovo", "ThinkPad E15", "NOTEBOOK: Lorem ipsum.", 10, 769.90, "i5-1135G7", "8GB", "SSD 256GB");
 var_dump($lenovoThinkpad);
+
+#User classes instance
+class User{
+    protected $name;
+    protected $surname;
+    protected $email;
+    protected $address;
+    
+    function __construct(string $name, string $surname, string $email, string $address){
+        $this-> name = $name;
+        $this-> surname = $surname;
+        $this-> email = $email;
+        $this-> address = $address;
+
+    }
+    public function addCreditCard($creditCard)
+    {
+        $this->creditCard = $creditCard;
+    }
+
+}
+class PremiumUser extends User {
+    protected $username;
+    protected $password;
+    protected $discount;
+
+    function __construct(string $name, string $surname, string $email, string $address, string $username, string $password, int $discount){
+        parent::__construct($name, $surname, $email, $address);
+        $this->username=$username;
+        $this->password=$password;
+        $this->discount=$discount;
+    }
+}
