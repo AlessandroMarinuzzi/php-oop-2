d784d332a96c584c38d886b7ad20d93f7483dc85
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-06-28 15:04:42 +0100

    - Create subclass Laptop with extended properties. - Invoke parent::__construct to create laptop product.

--- index.php
+++ index.php
@@ -17,3 +17,19 @@ class Product{
     }
 }
 
+class Laptop extends Product{
+    protected $intel;
+    protected $ram;
+    protected $ssd;
+
+    function __construct(string $brand, string $name, string $description, int $quantity, float $price, string $intel, string $ram, string $ssd)
+    {
+        parent::__construct($brand, $name, $description, $quantity, $price);
+        $this->intel= $intel;
+        $this->ram= $ram;
+        $this->ssd=$ssd;
+    }
+}
+
+$lenovoThinkpad = new Laptop ("Lenovo", "ThinkPad E15", "NOTEBOOK: Lorem ipsum.", 10, 769.90, "i5-1135G7", "8GB", "SSD 256GB");
+var_dump($lenovoThinkpad);
